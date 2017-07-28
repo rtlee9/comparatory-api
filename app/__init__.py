790962160120fcc15d291776ef54870bd5489517
Ryan Lee <ryantlee9@gmail.com>
2017-07-28 09:50:20 -0400

    S3 download based on config

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from config import set_config
 from flask_restful import Api
 from flat import extract_flat
 
-extract_flat()
 app = Flask(__name__)
 set_config(app)
+if app.config['S3_DOWNLOAD']:
+    extract_flat()
 
 api = Api(app)
 
