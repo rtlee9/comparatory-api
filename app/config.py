78be76d54ea5bcc0ca58c50a8d13b91f70646dd7
Ryan Lee <ryantlee9@gmail.com>
2017-07-31 22:26:41 -0400

    Remove Stormpath env variables

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 from os import path, environ
 from flask_bootstrap import Bootstrap
 from flask_sslify import SSLify
-from flask_stormpath import StormpathManager
 
 
 def set_config(app):
@@ -11,16 +10,6 @@ def set_config(app):
     SSLify(app)
     Bootstrap(app)
     app.config.from_object(environ['APP_SETTINGS'])
-
-    # OAuth credentials and configuration
-    app.config['SECRET_KEY'] = environ['STORMPATH_SECRET_KEY']
-    app.config['STORMPATH_API_KEY_ID'] = environ['STORMPATH_API_KEY_ID']
-    app.config['STORMPATH_API_KEY_SECRET'] = environ[
-        'STORMPATH_API_KEY_SECRET']
-    app.config['STORMPATH_APPLICATION'] = environ['STORMPATH_APPLICATION']
-    app.config['STORMPATH_ENABLE_MIDDLE_NAME'] = False
-    app.config['STORMPATH_ENABLE_FORGOT_PASSWORD'] = True
-    StormpathManager(app)
 
 
 path_app = path.dirname(path.abspath(__file__))
